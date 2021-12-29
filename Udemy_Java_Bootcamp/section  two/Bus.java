8e95e3189b6242b2182d26e57f3cf9c8e5354091
BryanHJH <2702bryan@gmail.com>
2021-12-29 12:59:51 +0800

    Completed the workbook 2.1 and added some new variables files

--- Udemy_Java_Bootcamp/section  two/Bus.java
+++ Udemy_Java_Bootcamp/section  two/Bus.java
@@ -1,11 +1,13 @@
 public class Bus {
     public static void main(String[] args) {
         int passengers = 0;
+        String sentence = "Number of passengers: ";
+
         passengers += 9;
-        System.out.println("Number of passengers: " + passengers);
+        System.out.println(sentence + passengers);
         passengers -= 5;
-        System.out.println("Number of passengers: " + passengers);
+        System.out.println(sentence + passengers);
         passengers -= 4;
-        System.out.println("Number of passengers: " + passengers);
+        System.out.println(sentence + passengers);
     }
 }
