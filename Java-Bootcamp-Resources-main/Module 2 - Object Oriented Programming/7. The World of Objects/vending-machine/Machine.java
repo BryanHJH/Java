0ca9e1c00374dd4965841bd4df9f50076a805d4d
BryanHJH <2702bryan@gmail.com>
2022-02-28 18:24:51 +0800

    Finished the Vending Machine challenge

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
@@ -1,6 +1,51 @@
+import java.util.Arrays;
+
 public class Machine {
+    private Item[][] items;
     
-    
+    //Constructors
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+
+        for (int k = 0; k < items.length; k++) {
+            for (int j2 = 0; j2 < items[k].length; j2++) {
+                if (items[k][j2] == null) {
+                    continue;
+                } else {
+                    this.items[k][j2] = new Item(items[k][j2]);
+                }
+            }
+        }
+    }
+
+    //Getters
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+
+    //Setter
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
+
+    //toString method
+    public String toString() {
+        String message = "";
+
+        for (int k = 0; k < items.length; k++) {
+            for (int j2 = 0; j2 < items.length; j2++) {
+                if (this.items[k][j2] == null) {
+                    message += "Sold out\t";
+                } else {
+                    message +="\t" + this.items[k][j2].toString() + " ";
+                }
+            }
+
+            message += "\n";
+        }
+
+        return message;
+    }
     
     /**
      * Function name – dispense
@@ -13,4 +58,14 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int spot) {
+        boolean result = false;
+
+        if (this.items[row][spot].getQuantity() > 0) {
+            this.items[row][spot].setQuantity(this.items[row][spot].getQuantity() - 1);
+            result = true;
+        }
+
+        return result;
+    }
 }
