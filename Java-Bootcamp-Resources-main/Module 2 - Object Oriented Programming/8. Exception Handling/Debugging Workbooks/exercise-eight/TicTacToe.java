72117c81527e649c865659f044da96a5d31cb053
Bryan Hor Jin Hao <2702bryan@gmail.com>
2022-03-07 23:11:39 +0800

    Completed some tutorials

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-eight/TicTacToe.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-eight/TicTacToe.java
@@ -7,21 +7,26 @@ public class TicTacToe {
         };
         
         for (int i = 0; i < array.length; i++) {
-               array[i][2-i] = 'O';
-               array[2-i][2-i] = 'X';
+                array[i][2-i] = 'O';
+
+                if (array[2-i][2-i] != '_') {
+                    continue;
+                } else {
+                    array[2-i][2-i] = 'X';
+                }
         }
 
         printArray(array);
         System.out.println("\nO WINS!\n");
-   }
-   
-   public static void printArray(char[][] array) {
-       System.out.println();
-       for (int i = 0; i < array.length; i++) {
-           for (int j = 0; j < array[i].length; j++) {
-               System.out.print( array[i][j] + "  ");
-           }
-           System.out.println();
-       }
-   }
+    }
+    
+    public static void printArray(char[][] array) {
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print( array[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
 }
